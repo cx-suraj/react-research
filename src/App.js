fd5bdb98416c26ab400f5feea948f7be1c2b0fa3
Suraj kushwaha <110454805+cx-suraj@users.noreply.github.com>
2023-03-17 11:53:23 +0530

    Code cleanup (#1)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import HomePDF from "./react-pdff/HomePDF";
-import Tickets from "./jspdf/Tickets";
-import { PdfPaint } from "./jspdf/withhtml2canvas/PdfPraint";
-import Pdfme from "./Pdfme/Pdfme";
-import DynamicPDF from "./Pdfme/DynamicPDF";
-import { Main } from "./CSV/Main";
 
 function App() {
   return (
     <div className="container">
-      {/* <HomePDF /> */}
-      {/* <Tickets /> */}
-      {/* Works with htm/css but have define fixed px html file*/}
-      {/* <PdfPaint /> */}
-      {/* <Pdfme /> */}
-      {/* <DynamicPDF/> */}
-      <Main />
     </div>
   );
 }
